cadae1eb4fdf41b3331a0c30144e18ed65b67200
Miguel Arredondo Calderon <miguel.arredondo@mad.vass.es>
2024-08-03 18:14:04 +0200

    Solucionado
    ddl-auto: update en vez de create-drop como esta probando.

--- inditex/src/test/java/com/bncn/inditex/InditexApplicationTests.java
+++ inditex/src/test/java/com/bncn/inditex/InditexApplicationTests.java
@@ -3,9 +3,12 @@ package com.bncn.inditex;
 import com.bncn.inditex.controller.ProductController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySources;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class InditexApplicationTests {
 //
     @Autowired
