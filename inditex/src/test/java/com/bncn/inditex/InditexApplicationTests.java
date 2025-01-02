ab06acdffca81e73517b66f520d02f2d2c86254e
Miguel Arredondo Calderón <miguel.arredondo@altia.es>
2025-01-02 23:22:31 +0100

    Solucionado todo.
    - En primer lugar el appliaction.yml de los test no era igual.
    - En segundo lugar, tenia que pasar de javax a jakarta.
      + Ademas, el openApi uso ApiUtil.java para los DTO y tenia que añadir <useJakartaEe> para que no cogiera javax
      + Todo esto es debido a las versiones 3.x que tengo de mi aplicacion y kafka (son medernas) y javax era incompatible.

--- inditex/src/test/java/com/bncn/inditex/InditexApplicationTests.java
+++ inditex/src/test/java/com/bncn/inditex/InditexApplicationTests.java
@@ -1,63 +1,63 @@
-//package com.bncn.inditex;
-//
-////import com.bncn.inditex.controller.ProductController;
+package com.bncn.inditex;
+
 //import com.bncn.inditex.controller.ProductController;
-//import com.bncn.inditex.exception.PriceNotFoundException;
-//import com.bncn.inditex.service.PriceService;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.TestPropertySources;
-//
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//
-//@SpringBootTest
-//@AutoConfigureTestDatabase
-//class InditexApplicationTests {
-//
-//    @Autowired
-//    ProductController productController;
-//    @Autowired
-//    PriceService priceService;
-//	@Test
-//	void testListProducts() throws Exception {
-////        try {
-////            productController.getPrices("2024-08-11 15:00:00",35455,1);
-////        } catch (Exception e) {
-////            throw new RuntimeException(e);
-////        }
-//        productController.getPrices("2024-08-11 15:00:00", 35455, 1);
-//    }
-//
-//    @Test
-//    void testListProductsNotFound() {
-////        try {
-////            productController.getPrices("2024-08-11 15:00:00",35456,1);
-////        } catch (Exception e) {
-////            assertThrows(PriceNotFoundException.class, () -> {
-////                priceService.findPrices(35456, 1, "2024-08-11 15:00:00");
-////            });
-////        }
-//        assertThrows(PriceNotFoundException.class, () ->
-//                productController.getPrices("2024-08-11 15:00:00", 35456, 1)
-//        );
-//    }
-//
-//    @Test
-//    void testListProductsInvalidParameters() {
-////        try {
-////            productController.getPrices("",35456,1);
-////        } catch (Exception e) {
-////            assertThrows(PriceNotFoundException.class, () -> {
-////                priceService.findPrices(35456, 1, "");
-////            });
-////        }
-//        assertThrows(PriceNotFoundException.class, () ->
-//                productController.getPrices("", 35456, 1)
-//        );
-//    }
-//
-//
-//
-//}
+import com.bncn.inditex.controller.ProductController;
+import com.bncn.inditex.exception.PriceNotFoundException;
+import com.bncn.inditex.service.PriceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySources;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+class InditexApplicationTests {
+
+    @Autowired
+    ProductController productController;
+    @Autowired
+    PriceService priceService;
+	@Test
+	void testListProducts() throws Exception {
+//        try {
+//            productController.getPrices("2024-08-11 15:00:00",35455,1);
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+        productController.getPrices("2024-08-11 15:00:00", 35455, 1);
+    }
+
+    @Test
+    void testListProductsNotFound() {
+//        try {
+//            productController.getPrices("2024-08-11 15:00:00",35456,1);
+//        } catch (Exception e) {
+//            assertThrows(PriceNotFoundException.class, () -> {
+//                priceService.findPrices(35456, 1, "2024-08-11 15:00:00");
+//            });
+//        }
+        assertThrows(PriceNotFoundException.class, () ->
+                productController.getPrices("2024-08-11 15:00:00", 35456, 1)
+        );
+    }
+
+    @Test
+    void testListProductsInvalidParameters() {
+//        try {
+//            productController.getPrices("",35456,1);
+//        } catch (Exception e) {
+//            assertThrows(PriceNotFoundException.class, () -> {
+//                priceService.findPrices(35456, 1, "");
+//            });
+//        }
+        assertThrows(PriceNotFoundException.class, () ->
+                productController.getPrices("", 35456, 1)
+        );
+    }
+
+
+
+}
